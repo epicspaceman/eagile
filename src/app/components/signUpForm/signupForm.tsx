005f1060d7727147d01745acf4931f94de111f46
Robert Lockay <robert.lockay@gmail.com>
2025-04-21 12:39:54 -0500

    signup styling good

--- src/app/components/signUpForm/signupForm.tsx
+++ src/app/components/signUpForm/signupForm.tsx
@@ -7,15 +7,21 @@ const SignupForm = () => {
     const [state, action] = useActionState(signup, undefined)
 
     return (
-        <form action={action}>
-            <h1>Become a scavenger.</h1>
-                <div>
-                    <label>Username</label>
-                    <input type="text" id="username" name="username"/>
+        <form className='flex flex-col gap-5 p-8 rounded-lg w-1/4 shadow-[0_1px_10px_rgba(0,0,0,0.2)]' action={action}>
+            <div className='flex flex-col gap-1'>
+                <h1 className='text-5xl text-french-purple text-center'>Eagile</h1>
+                <p className='text-s text-gray-500 text-center'>Become an agile eagle</p>
+            </div>
+            <div className='flex flex-col gap-5'>
+                <div className='flex flex-col'>
+                    <label className='text-gray-500'>Username</label>
+                    <input className='bg-gray-200 p-1 rounded-lg' type="text" id="username" name="username"/>
                     {state?.errors?.username && <p>{state.errors.username}</p>}
+                </div>
 
-                    <label>Password</label>
-                    <input type="password" id="password" name="password"/>
+                <div className='flex flex-col'>
+                    <label className='text-gray-500'>Password</label>
+                    <input className='bg-gray-200 p-1 rounded-lg' type="password" id="password" name="password"/>
                     {state?.errors?.password && (
                         <div>
                             <p>Password must:</p>
@@ -27,7 +33,8 @@ const SignupForm = () => {
                         </div>
                     )}
                 </div>
-            <button type="submit">Sign Up</button>
+            </div>
+            <button className='text-white bg-french-purple w-1/2 self-center rounded-lg mt-1 p-1' type="submit">Sign Up</button>
         </form>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,8 @@ import SignupForm from '@/app/components/signUpForm/signupForm';
 
 const SignUp = () => {
     return (
-        <main>
-            <div>
+        <main className='flex border-red-500 border-4 border-dotted h-screen w-full'>
+            <div className='content-center justify-items-center border-red-500 border-4 border-dotted w-full'>
                 <SignupForm />
             </div>
         </main>
