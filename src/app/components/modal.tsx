8c5c5db05062f920251675988cbca5e273d4bff9
Robert Lockay <robert.lockay@gmail.com>
2025-04-21 10:10:14 -0500

    z50

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -11,7 +11,7 @@ const Modal = ({ isOpen, children }: Props) => {
     if (!isOpen) return null
 
     return(
-        <div className="bg-black/50 flex items-center justify-center w-full h-full fixed inset-0">
+        <div className="bg-black/50 flex items-center justify-center w-full h-full fixed inset-0 z-50">
             <div className="bg-white p-2 w-1/3 h-fit rounded-lg overflow-scroll">
                 {children}
             </div>
